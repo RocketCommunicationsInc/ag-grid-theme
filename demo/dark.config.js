87d1bdbfb81749c745cde25d4a33e99a6ff96aa7
Mark Anthony Cianfrani <markacianf@gmail.com>
2022-03-28 14:52:46 -0400

    refactor: beta tokens

--- demo/dark.config.js
+++ demo/dark.config.js
@@ -1,35 +1,37 @@
 const baseUrl = "http://localhost:3000/";
 
-const scenarios = [{
-  label: 'home',
-  url: baseUrl,
-  misMatchThreshold: 1e-10,
-  requireSameDimensions: false,
-}];
+const scenarios = [
+	{
+		label: 'home',
+		url: baseUrl,
+		misMatchThreshold: 1e-10,
+		requireSameDimensions: false,
+	}
+];
 
 const projectId = "dark";
 module.exports = {
-  id: projectId,
-  scenarios,
-  paths: {
-    bitmaps_reference: `backstop_data/snapshots/${projectId}`,
-    bitmaps_test: `backstop_data/bitmaps_test/${projectId}`,
-    ci_report: "backstop_data/ci_report",
-    engine_scripts: "backstop_data/engine_scripts",
-    html_report: `backstop_data/html_report/${projectId}`,
-  },
-  report: ["browser"],
-  engine: "puppeteer",
-  engineOptions: {
-    args: ["--no-sandbox"],
-  },
-  asyncCaptureLimit: 10,
-  asyncCompareLimit: 100,
-  viewports: [
-    {
-      height: 768,
-      label: "PC",
-      width: 1920,
-    },
-  ],
+	id: projectId,
+	scenarios,
+	paths: {
+		bitmaps_reference: `backstop_data/snapshots/${projectId}`,
+		bitmaps_test: `backstop_data/bitmaps_test/${projectId}`,
+		ci_report: "backstop_data/ci_report",
+		engine_scripts: "backstop_data/engine_scripts",
+		html_report: `backstop_data/html_report/${projectId}`,
+	},
+	report: ["browser"],
+	engine: "puppeteer",
+	engineOptions: {
+		args: ["--no-sandbox"],
+	},
+	asyncCaptureLimit: 10,
+	asyncCompareLimit: 100,
+	viewports: [
+		{
+			height: 768,
+			label: "PC",
+			width: 1920,
+		},
+	],
 };
